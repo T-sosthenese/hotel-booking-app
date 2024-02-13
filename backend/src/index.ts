0c6236e6db4450eb13aa5fd3b3c2d6d84da7e797
T-sosthenese <sosthenetimi94@gmail.com>
2024-02-13 15:49:36 +0300

    Compiled frontend and backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,21 +5,13 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
-
-// dotenv.config(); // Load environment variables from .env file
+import path from "path";
 
 if (process.env.NODE_ENV === "e2e") {
   dotenv.config({ path: ".env.e2e" });
 } else {
   dotenv.config(); // This will load .env by default
 }
-
-// const uri = process.env.MONGODB_CONNECTION_STRING;
-// if (!uri) {
-//   throw new Error(
-//     "Please define the MONGODB_CONNECTION_STRING environment variable"
-//   );
-// }
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -33,6 +25,8 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
