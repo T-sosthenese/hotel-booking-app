4db507247619af7f5b9623f34d5ca07cb70d39d3
T-sosthenese <sosthenetimi94@gmail.com>
2024-02-26 15:25:13 +0300

    Checking bugs in the user type definition

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,28 +1,24 @@
-import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
+import mongoose, { Document, Schema, Types } from "mongoose";
 
 export type UserType = {
-  _id: string;
+  _id?: Types.ObjectId;
   email: string;
   password: string;
   firstName: string;
   lastName: string;
 };
 
-const userSchema = new mongoose.Schema({
+interface UserDocument extends Omit<UserType, "_id">, Document {
+  _id: Types.ObjectId;
+}
+
+const userSchema = new Schema<UserDocument>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
 });
 
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 8);
-  }
-  next();
-});
-
-const User = mongoose.model<UserType>("User", userSchema);
+const User = mongoose.model<UserDocument>("User", userSchema);
 
 export default User;
