0151ae53b782eaf2d2b272324a17992c5be9deff
T-sosthenese <sosthenetimi94@gmail.com>
2024-03-04 12:34:36 +0300

    Changed UI colors

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,7 +5,7 @@ import SignOutButton from "./SignOutButton";
 const Header = () => {
   const { isLoggedIn } = useAppContext();
   return (
-    <div className="bg-orange-400 py-6">
+    <div className="bg-blue-800 py-6">
       <div className="container mx-auto flex justify-between">
         <span className="text-3xl text-white font-bold tracking-tight">
           <Link to="/">comfortcrib.com</Link>
